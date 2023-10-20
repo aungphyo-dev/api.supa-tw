c80bbacbd3c3a304a9565d5645ceff9a7b28e863
typle <aungpyaephyo1412@gmail.com>
2023-10-20 18:19:27 +0630

    Hee Hee V1

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
-Route::prefix("auth")->controller(\App\Http\Controllers\Api\AuthController::class)->group(function (){
-    Route::post("register","register");
-    Route::post("login","login");
-    Route::post("logout","logout");
-    Route::get("profile","profile");
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
