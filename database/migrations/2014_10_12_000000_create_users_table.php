124c34fda3f2d708b46da671028e875d9e7e2bd2
typle <aungpyaephyo1412@gmail.com>
2023-10-20 19:56:12 +0630

    auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable()->unique();
+            $table->text("bio")->nullable();
+            $table->text("location")->nullable();
+            $table->string("website")->nullable();
+            $table->string("profile_avatar")->nullable();
+            $table->string("cover_avatar")->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum("role",["user","admin"])->default("user");
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix("auth")->controller(\App\Http\Controllers\AuthController::class)->middleware("api")->group(function (){
+    Route::post("register","register");
+    Route::post('login', 'login');
+    Route::post('logout', 'logout');
+    Route::get('profile', 'profile');
 });
