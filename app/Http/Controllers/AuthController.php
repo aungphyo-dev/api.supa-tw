ebdc775c3cc4324917c03555e3d1ace3ce5aa3d6
typle <aungpyaephyo1412@gmail.com>
2023-10-28 17:49:03 +0630

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Following;
+use App\Models\Tweet;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -71,12 +73,93 @@ class AuthController extends Controller
         return response()->json([
             'status' => 'success',
             'message' => 'User detail!',
-            'user' => new UserResource($user)
+            'user' => new UserResource($user),
         ]);
     }
 
     public function update (Request $request)
     {
+        $user = User::find(Auth::id());
+        $request->validate([
+            "name" => "max:50",
+            "bio" => "max:150",
+            "location" => "max:100",
+        ]);
         return $request;
+        if (!$request->name && !$request->bio && !$request->location && !$request->website && !$request->profile_avatar && !$request->cover_avatar){
+            return response()->json([
+                "error" => "Your are not update!"
+            ]);
+        }
+
+        if ($request->hasFile("profile_avatar")) {
+            $photo = $request->file("profile_avatar");
+            $extension = $photo->extension();
+            $photoName = "user_photo" . time() . rand(0,100000) . "." . $extension;
+            $photo->storeAs("users", $photoName, "public");
+            $user->profile_avatar = $photoName;
+        }
+        if ($request->hasFile("cover_avatar")) {
+            $photo = $request->file("cover_avatar");
+            $extension = $photo->extension();
+            $photoName = "user_photo" . time() . rand(0,100000) . "." . $extension;
+            $photo->storeAs("users", $photoName, "public");
+            $user->cover_avatar = $photoName;
+        }
+
+        if ($request->name){
+            $user->name = $request->name;
+        }
+        if ($request->bio){
+            $user->bio = $request->bio;
+        }
+        if ($request->website){
+            $user->website = $request->website;
+        }
+        if ($request->location){
+            $user->location = $request->location;
+        }
+        $user->update();
+        return  response()->json([
+            "success" => "User successfully updated"
+        ]);
+    }
+
+    public function index ()
+    {
+        $users = User::with("followers")->latest("id")->get();
+        return response()->json(compact("users"));
+    }
+
+    public function followingTweet()
+    {
+        $followingIds = Following::where("follower_id",Auth::id())->pluck("following_id");
+        $tweets = Tweet::with("author")->whereIn("user_id",$followingIds)->paginate(20);
+        return response()->json(compact("tweets"));
+    }
+    public function followingByAuth()
+    {
+        $followings = Following::with("followingUser","followerUser")->where("follower_id",Auth::id())->get();
+        return response()->json(compact("followings"));
+    }
+    public function followersByAuth()
+    {
+        $followers = Following::with("followingUser","followerUser")->where("following_id",Auth::id())->get();
+        return response()->json(compact("followers"));
+    }
+     public function UserById(string $id)
+    {
+        $user = User::find($id);
+        if ( is_null($user)){
+            return response()->json([
+                "error" => "User not found",
+            ]);
+        };
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User detail!',
+            'user' => new UserResource($user)
+        ]);
     }
 }
