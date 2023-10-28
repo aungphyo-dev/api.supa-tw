e180542c28790e7c4fd1fd058ce3f3b5eaf91e32
typle <aungpyaephyo1412@gmail.com>
2023-10-28 17:49:42 +0630

    following

--- app/Models/Following.php
+++ app/Models/Following.php
@@ -5,8 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Following extends Model
 {
     use HasFactory;
+
+    public function followerUser () : BelongsTo
+    {
+        return $this->belongsTo(User::class,"follower_id","id");
+    }
+
+    public function followingUser () : BelongsTo
+    {
+        return $this->belongsTo(User::class,"following_id","id");
+    }
 }
