70e6a6e2499668616c738d4848985c603bb7c674
typle <aungpyaephyo1412@gmail.com>
2023-10-31 16:07:55 +0630

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(20)->create();
-//        Tweet::factory(100)->create();
-//        Following::factory(20)->create();
+//          \App\Models\User::factory(20)->create();
+//       Tweet::factory(100)->create();
+//       Following::factory(20)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
