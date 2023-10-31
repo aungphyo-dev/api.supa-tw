0e75a5894afb893c4f860d48d2dce4ab6773ea0e
typle <aungpyaephyo1412@gmail.com>
2023-10-31 16:07:36 +0630

    like

--- database/migrations/2023_10_23_152106_create_likes_table.php
+++ database/migrations/2023_10_23_152106_create_likes_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("tweet_id")->references("id")->on("tweets")->cascadeOnDelete();
+            $table->foreignId("like_user_id")->references("id")->on("users")->cascadeOnDelete();
             $table->timestamps();
         });
     }
